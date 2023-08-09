622b836c9c933784cf64c56ec8568b96cf5dfa30
GregoPonce <gregorio.ponce213612@alumnos.info.unlp.edu.ar>
2023-08-09 10:13:07 -0300

    ds

--- script/toggle.js
+++ script/toggle.js
@@ -2,18 +2,22 @@ const toggleBtn = document.querySelector('.toggle');
 const toggle = document.querySelector('.toggle');
 const toggleIcon = document.querySelector('.toggle i');
 const dropdown = document.querySelector('.dropdown');
-const lista = document.querySelectorAll('.elementos-lista');
+const lista = document.querySelector('.elementos-lista');
 
 toggle.onclick = function () {
-    dropdown.classList.add('open');
+    dropdown.classList.toggle('open'); // Cambiar 'add' por 'toggle'
     const isOpen = dropdown.classList.contains('open');
 
     toggleIcon.classList = isOpen
         ? 'fa-solid fa-xmark'
-        : 'fa-solid fa-bars'
-}
+        : 'fa-solid fa-bars';
+};
 
-lista.onclick = function () {
+// Agregar manejador de eventos a los enlaces <a>
+const elementosListaLinks = document.querySelectorAll('.elementos-lista a');
+elementosListaLinks.forEach(link => {
+    link.addEventListener('click', function () {
         dropdown.classList.remove('open');
-        console.log('prueba');
-}
+        toggleIcon.classList = 'fa-solid fa-bars';
+    });
+});
