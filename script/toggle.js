2e2aa13a89013e3f6632eeac57ea244ce577211d
Emiliano Leyva <emilianoleyva@yahoo.com.ar>
2023-08-09 09:29:49 -0300

    df

--- script/toggle.js
+++ script/toggle.js
@@ -1,13 +1,19 @@
-const toggleBtn = document.querySelector('.toggle')
-const toggle = document.querySelector('.toggle')
-const toggleIcon = document.querySelector('.toggle i')
-const dropdown = document.querySelector('.dropdown')
+const toggleBtn = document.querySelector('.toggle');
+const toggle = document.querySelector('.toggle');
+const toggleIcon = document.querySelector('.toggle i');
+const dropdown = document.querySelector('.dropdown');
+const lista = document.querySelectorAll('.elementos-lista');
 
-toggle.onclick = function (){
-    dropdown.classList.toggle('open')
-    const isOpen = dropdown.classList.contains('open')
+toggle.onclick = function () {
+    dropdown.classList.add('open');
+    const isOpen = dropdown.classList.contains('open');
 
     toggleIcon.classList = isOpen
         ? 'fa-solid fa-xmark'
         : 'fa-solid fa-bars'
+}
+
+lista.onclick = function () {
+        dropdown.classList.remove('open');
+        console.log('prueba');
 }
